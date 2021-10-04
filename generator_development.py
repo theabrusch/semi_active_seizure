52da6cd062c3d16d10a5a47e29b6b1a18df52525
theb <s164238@student.dtu.dk>
2021-10-04 16:58:54 +0200

    initial model

--- generator_development.py
+++ generator_development.py
@@ -1,14 +1,19 @@
 from dataapi import data_collection as dc
-from h5py._hl import dataset
 import numpy as np
-#from src.data import datagenerator
+from src.data import datagenerator
 from torch.utils.data import DataLoader, WeightedRandomSampler
-import datagenerator
+from src.models import baselinemodels
+import torch
 
 dataset = datagenerator.DataGenerator('data/hdf5/temple_seiz.hdf5', 
-                                      window_length = 4, stride = [4, 2], 
+                                      window_length = 2, 
                                       protocol = 'test', signal_name = 'TCP', 
                                       bckg_rate = 1, anno_based_seg=True)
 weights = dataset.samples['weight']
 sampler = WeightedRandomSampler(weights, num_samples=dataset.__len__(), replacement = True)
 dataloader = DataLoader(dataset, batch_size=32, sampler=sampler)
+temp = next(iter(dataloader))
+
+
+model = baselinemodels.BaselineCNN(input_shape=(20,500))
+out = model(torch.Tensor(temp[0]))
--- src/models/baselinemodels.py
+++ src/models/baselinemodels.py
@@ -3,7 +3,7 @@ import torch.nn.functional as F
 import numpy as np
 import pandas as pd
 
-def conv_size(input_size, kernel, padding, stride):
+def conv_size(input_size, kernel, padding, stride=1):
     '''
     Return output shape after convolution based on input
     size, kernel size, padding and stride. 
@@ -15,15 +15,49 @@ def conv_size(input_size, kernel, padding, stride):
     if isinstance(stride, int):
         stride = (stride, stride)
     h,w = input_size
-    h_out = (h + 2*padding[0] - kernel[0])/stride[0] + 1
-    w_out = (w + 2*padding[1] - kernel[1])/stride[1] + 1
+    h_out = int((h + 2*padding[0] - kernel[0])/stride[0]) + 1
+    w_out = int((w + 2*padding[1] - kernel[1])/stride[1]) + 1
 
-    return (h_out, w_out)
-
-
-
+    return h_out, w_out
 
 class BaselineCNN(nn.Module):
 
-    def __init__(self):
-        super(BaselineCNN, self).__init__()
+    def __init__(self, input_shape):
+        super(BaselineCNN, self).__init__() 
+
+        h, w = conv_size(input_shape, (1,2), 0, stride=(1,2))
+        h, w = conv_size((h,w), (1,2), 0, stride=(1,2))
+        h, w = conv_size((h,w), 2, 0, stride=2)
+        h, w = conv_size((h,w), 3, 0, stride=1)
+
+        self.convblock = nn.Sequential(
+            nn.Conv2d(in_channels = 1, out_channels = 20, 
+                      kernel_size = (1,11), padding = 'same'),
+            nn.ELU(),
+            nn.BatchNorm2d(20),
+            nn.Conv2d(in_channels = 20, out_channels = 20, 
+                      kernel_size=(input_shape[0], 21), padding = 'same'),
+            nn.ELU(),
+            nn.BatchNorm2d(20),
+            nn.MaxPool2d(kernel_size = (1,2), stride = (1,2)),
+            nn.Conv2d(in_channels = 20, out_channels = 40, 
+                      kernel_size = (11, 21), padding = 'same'),
+            nn.ELU(),
+            nn.BatchNorm2d(40),
+            nn.MaxPool2d(kernel_size = (1,2), stride = (1,2)),
+            nn.Conv2d(in_channels = 40, out_channels=80, 
+                      kernel_size = (11, 11), padding = 'same'),
+            nn.ELU(),
+            nn.BatchNorm2d(80),
+            nn.MaxPool2d(kernel_size=(2,2), stride = (2,2)),
+            nn.Conv2d(in_channels = 80, out_channels=2, 
+                      kernel_size=(3, 3), padding = 'valid'),
+        )
+        self.GlobAvgPool = nn.AvgPool2d(kernel_size=(h,w), stride =(h,w))
+    
+    def forward(self,x):
+        out = self.convblock(x.unsqueeze(1))
+        out = self.GlobAvgPool(out)
+        out = out.squeeze(3).squeeze(2)
+        out = F.softmax(out)
+        return out
