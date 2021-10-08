96bbdd58917139c4ac44745732fc4d642c3a9973
theb <s164238@student.dtu.dk>
2021-10-08 15:31:59 +0200

    add fc layer and dropout

--- src/models/baselinemodels.py
+++ src/models/baselinemodels.py
@@ -28,39 +28,40 @@ class BaselineCNN(nn.Module):
 
         h, w = conv_size(input_shape, (1,2), 0, stride=(1,2))
         h, w = conv_size((h,w), (1,2), 0, stride=(1,2))
-        h, w = conv_size((h,w), 2, 0, stride=2)
-        h, w = conv_size((h,w), 3, 0, stride=1)
-
+        
         ch_dim = [input_shape[0] if input_shape[0]%2 != 0 else input_shape[0]-1][0]
 
         self.convblock = nn.Sequential(
             nn.Conv2d(in_channels = 1, out_channels = 20, 
                       kernel_size = (1,11), padding = 'same'),
             nn.ELU(),
+            nn.Dropout(0.4),
             nn.BatchNorm2d(20),
             nn.Conv2d(in_channels = 20, out_channels = 20, 
                       kernel_size=(ch_dim, 21), padding = 'same'),
             nn.ELU(),
+            nn.Dropout(0.4),
             nn.BatchNorm2d(20),
             nn.MaxPool2d(kernel_size = (1,2), stride = (1,2)),
             nn.Conv2d(in_channels = 20, out_channels = 40, 
                       kernel_size = (11, 21), padding = 'same'),
             nn.ELU(),
+            nn.Dropout(0.4),
             nn.BatchNorm2d(40),
             nn.MaxPool2d(kernel_size = (1,2), stride = (1,2)),
             nn.Conv2d(in_channels = 40, out_channels=80, 
-                      kernel_size = (11, 11), padding = 'same'),
-            nn.ELU(),
-            nn.BatchNorm2d(80),
-            nn.MaxPool2d(kernel_size=(2,2), stride = (2,2)),
-            nn.Conv2d(in_channels = 80, out_channels=2, 
-                      kernel_size=(3, 3), padding = 'valid'),
+                      kernel_size = (11, 11), padding = 'same')
         )
+        self.dropout = nn.Dropout(0.4)
         self.GlobAvgPool = nn.AvgPool2d(kernel_size=(h,w), stride =(h,w))
+        self.fc = nn.Linear(in_features=80, out_features=2)
     
     def forward(self,x):
-        out = self.convblock(x.unsqueeze(1))
-        out = self.GlobAvgPool(out)
-        out = out.squeeze(3).squeeze(2)
-        out = F.softmax(out, dim = 1)
+        x = self.convblock(x.unsqueeze(1))
+        x = self.GlobAvgPool(x)
+        x = F.elu(x)
+        x = self.dropout(x)
+        x = x.squeeze(3).squeeze(2)
+        x = self.fc(x)
+        out = F.softmax(x, dim = 1)
         return out
