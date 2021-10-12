ac601cd6a19327a1df418ed9e137d0b310dba1d0
Thea Brusch <s164238@student.dtu.dk>
2021-10-12 15:56:32 +0200

    first full run

--- job.sh
+++ job.sh
@@ -7,10 +7,10 @@
 ### specify that all cores should be on the same host
 #BSUB -R "span[hosts=1]"
 ### specify the memory needed
-#BSUB -R "rusage[mem=100GB]"
+#BSUB -R "rusage[mem=200GB]"
 ### Number of hours needed
 #BSUB -N 
-#BSUB -W 05:00
+#BSUB -W 24:00
 ### added outputs and errors to files
 #BSUB -o logs/Output_%J.out
 #BSUB -e logs/Error_%J.err
