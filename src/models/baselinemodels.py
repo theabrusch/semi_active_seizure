f0b87ee2ff52a5c0543cc563633c32f017393b41
theb <s164238@student.dtu.dk>
2021-10-13 16:51:08 +0200

    adjust model

--- src/models/baselinemodels.py
+++ src/models/baselinemodels.py
@@ -23,45 +23,57 @@ def conv_size(input_size, kernel, padding, stride=1):
 
 class BaselineCNN(nn.Module):
 
-    def __init__(self, input_shape, **kwargs):
+    def __init__(self, input_shape, dropoutprob = 0.2, padding=True, **kwargs):
         super(BaselineCNN, self).__init__() 
 
-        h, w = conv_size(input_shape, (1,2), 0, stride=(1,2))
-        h, w = conv_size((h,w), (1,2), 0, stride=(1,2))
-        
         ch_dim = [input_shape[0] if input_shape[0]%2 != 0 else input_shape[0]-1][0]
+
+        if padding:
+            h, w = conv_size(input_shape, (1,2), 0, stride=(1,2))
+            h, w = conv_size((h,w), (1,2), 0, stride=(1,2))
+        else:
+            h, w = conv_size(input_shape, (1, 10), 0, stride = 1)
+            h, w = conv_size((h,w), (ch_dim, 1), 0, stride = 1)
+            h, w = conv_size((h,w), (1,2), 0, stride=(1,2))
+            h, w = conv_size((h,w), (10,10), 0, stride=1)
+            h, w = conv_size((h,w), (1,2), 0, stride=(1,2))
+            h, w = conv_size((h,w), (10,10), 0, stride=1)
+            
+        if padding:
+            padding = 'same'
+        else:
+            padding = 'valid'
 
         self.convblock = nn.Sequential(
             nn.Conv2d(in_channels = 1, out_channels = 20, 
-                      kernel_size = (1,11), padding = 'same'),
+                      kernel_size = (1,10), padding = padding),
             nn.ELU(),
-            nn.Dropout(0.4),
+            nn.Dropout(dropoutprob),
             nn.BatchNorm2d(20),
             nn.Conv2d(in_channels = 20, out_channels = 20, 
-                      kernel_size=(ch_dim, 21), padding = 'same'),
+                      kernel_size=(ch_dim, 1), padding = padding),
             nn.ELU(),
-            nn.Dropout(0.4),
+            nn.Dropout(dropoutprob),
             nn.BatchNorm2d(20),
             nn.MaxPool2d(kernel_size = (1,2), stride = (1,2)),
             nn.Conv2d(in_channels = 20, out_channels = 40, 
-                      kernel_size = (11, 21), padding = 'same'),
+                      kernel_size = (10, 10), padding = padding),
             nn.ELU(),
-            nn.Dropout(0.4),
+            nn.Dropout(dropoutprob),
             nn.BatchNorm2d(40),
             nn.MaxPool2d(kernel_size = (1,2), stride = (1,2)),
             nn.Conv2d(in_channels = 40, out_channels=80, 
-                      kernel_size = (11, 11), padding = 'same')
+                      kernel_size = (10, 10), padding = padding)
         )
-        self.dropout = nn.Dropout(0.4)
-        self.GlobAvgPool = nn.AvgPool2d(kernel_size=(h,w), stride =(h,w))
-        self.fc = nn.Linear(in_features=80, out_features=2)
+        self.flatten = nn.Flatten()
+        self.dropout = nn.Dropout(dropoutprob)
+        self.fc = nn.Linear(in_features=h*w*80, out_features=2)
     
     def forward(self,x):
         x = self.convblock(x.unsqueeze(1))
-        x = self.GlobAvgPool(x)
+        x = self.flatten(x)
         x = F.elu(x)
         x = self.dropout(x)
-        x = x.squeeze(3).squeeze(2)
         x = self.fc(x)
         out = F.softmax(x, dim = 1)
         return out
