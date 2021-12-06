1ec57127c41f6ddf21c7c00a58e3564a6250c158
theb <s164238@student.dtu.dk>
2021-12-06 09:23:40 +0100

    new optimizer

--- src/models/get_optim.py
+++ src/models/get_optim.py
@@ -1,9 +1,15 @@
+from os import error
 import torch.optim as optim
 
 def get_optim(model_params, optim_kwargs):
     if optim_kwargs['optimizer'] == 'Adam':
         optimizer = optim.Adam(model_params, lr = optim_kwargs['lr'],
                                weight_decay=optim_kwargs['weight_decay'])
+    elif optim_kwargs['optimizer'] == 'RMSprop':
+        optimizer = optim.RMSprop(model_params, lr = optim_kwargs['lr'], 
+                                  weight_decay=optim_kwargs['weight_decay'])
+    else:
+        raise ValueError('Available optimizers are Adam and RMSprop.')
     
     if optim_kwargs['scheduler'] == 'MultistepLR':
         scheduler = optim.lr_scheduler.MultiStepLR(optimizer, optim_kwargs['milestones'])
