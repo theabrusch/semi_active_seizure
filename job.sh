6df8ea660dde09f073b45d63e7e46d6b6e4822f7
Thea Brusch <s164238@student.dtu.dk>
2021-12-08 18:03:00 +0100

    job script

--- job.sh
+++ job.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #BSUB -q gpuv100
 #BSUB -gpu "num=1"
-#BSUB -J bckg_rate_10
+#BSUB -J upsample_seiz
 ### number of core
 #BSUB -n 1 
 ### specify that all cores should be on the same host
@@ -21,5 +21,5 @@ source $HOME/miniconda3/bin/activate
 conda activate semi_active_seiz
 nvidia-smi > ./gpu_stats.csv
 nvidia-smi --query-gpu=index,gpu_name,memory.total,memory.used,memory.free,temperature.gpu,pstate,utilization.gpu,utilization.memory --format=csv -l 10 >> ./gpu_stats.csv &
-python3 main.py --file_path /work3/theb/boston_scalp_new.hdf5 --bckg_stride 1 --seiz_stride 1 --bckg_rate_val 1 --bckg_rate_train 1 --lr 1e-5 --prefetch_data_from_seg True --epochs 200 --weight_decay 1e-2 --train_val_test True --use_weighted_loss False --padding True --standardise True --anno_based_seg True --dropoutprob 0.4
+python3 main.py --file_path /work3/theb/boston_scalp_new.hdf5 --bckg_stride 2 --seiz_stride 1 --bckg_rate_val 2 --bckg_rate_train 2 --lr 1e-5 --prefetch_data_from_seg True --epochs 100 --weight_decay 1e-2 --train_val_test True --use_weighted_loss True --padding True --standardise False --anno_based_seg True --dropoutprob 0.6
 pkill nvidia-smi
