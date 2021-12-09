6a719c8d7a119a9bb883b07c51fbfaf6cb3a27c3
Thea Brusch <s164238@student.dtu.dk>
2021-12-09 13:22:37 +0100

    job script

--- job_attbilstm.sh
+++ job_attbilstm.sh
@@ -21,5 +21,5 @@ source $HOME/miniconda3/bin/activate
 conda activate semi_active_seiz
 nvidia-smi > ./gpu_stats.csv
 nvidia-smi --query-gpu=index,gpu_name,memory.total,memory.used,memory.free,temperature.gpu,pstate,utilization.gpu,utilization.memory --format=csv -l 10 >> ./gpu_stats.csv &
-python3 main.py --file_path /work3/theb/boston_scalp_new.hdf5 --bckg_stride 23 --seiz_stride 23 --bckg_rate_val 1 --bckg_rate_train 1 --lr 0.0013 --prefetch_data_from_seg True --epochs 50 --weight_decay 0.02 --train_val_test False --use_weighted_loss False --padding True --standardise False --anno_based_seg False --dropoutprob 0.4 --window_length 23 --model_type AttBiLSTM
+python3 main.py --file_path /work3/theb/boston_scalp.hdf5 --bckg_stride 23 --seiz_stride 23 --bckg_rate_val 1 --bckg_rate_train 1 --lr 0.0013 --prefetch_data_from_seg True --epochs 50 --weight_decay 0.02 --train_val_test False --use_weighted_loss False --padding True --standardise False --anno_based_seg False --dropoutprob 0.4 --window_length 23 --model_type AttBiLSTM
 pkill nvidia-smi
