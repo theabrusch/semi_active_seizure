6d7805654a50f03268a0ca8ce2ecc5e59261117f
theb <s164238@student.dtu.dk>
2021-12-27 11:03:12 +0100

    check hdf5 files

--- temp.py
+++ temp.py
@@ -2,23 +2,35 @@ import numpy as np
 import pyedflib
 from dataapi import data_collection as dc
 
-file_name = '/Users/theabrusch/Desktop/Speciale_data/hdf5/temple_seiz.hdf5'
+file_name = '/Users/theabrusch/Desktop/Speciale_data/hdf5/boston_scalp.hdf5'
 
 f = dc.File(file_name, 'r')
 
 seiz_subjs = []
 seiz_dur = 0
 total_dur = 0
+stats = dict()
 
 for subj in f['train'].keys():
     subject = f['train'][subj]
-    seiz = False
+    stats[subj] = dict()
+    stats[subj]['total dur'] = 0
+    stats[subj]['seizures'] = 0
+    stats[subj]['seiz dur'] = 0
     for rec in subject.keys():
         record = subject[rec]
         for anno in record['Annotations']:
             if anno['Name'] in ['cpsz', 'gnsz', 'spsz', 'tcsz', 'seiz']:
                 seiz = True
-                seiz_dur += anno['Duration']
-        total_dur+=record.duration
-    if seiz:
-        seiz_subjs.append(subj)
+                stats[subj]['seizures'] += 1
+                stats[subj]['seiz dur'] += anno['Duration']
+        stats[subj]['total dur'] += record.duration
+
+
+for subj in stats.keys():
+    print('Subject', subj)
+    print('Number of seizures', stats[subj]['seizures'])
+    print('Seizure duration', stats[subj]['seiz dur'])
+    print('Seizure percent', stats[subj]['seiz dur']*100/stats[subj]['total dur'])
+    print('Total duration', stats[subj]['total dur']/60/60)
+    print('\n')
