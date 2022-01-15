b232223ba66ea66cf402d80d100e316431df6a60
theb <s164238@student.dtu.dk>
2022-01-15 17:18:04 +0100

    take seed as input

--- cross_val_main.py
+++ cross_val_main.py
@@ -54,6 +54,7 @@ def main(args):
     splitdict['hdf5_path'] = args.filde_path
     splitdict['seiz_strat'] = args.seiz_strat
     splitdict['protocol'] = 'all'
+    splitdict['seed'] = args.seed
     # get split
     split_path = 'data/optuna_trials/optuna_split_temple/' 
     p = Path(split_path)
@@ -184,6 +185,7 @@ if __name__ == '__main__':
     parser.add_argument('--job_name', type = str, default='nojobname')
     parser.add_argument('--run_folder', type = str, default='notspec')
     # datagen
+    parser.add_argument('--seed', type = int, default = 20)
     parser.add_argument('--seiz_classes', nargs='+', default=['fnsz', 'gnsz', 'cpsz', 'spsz', 'tcsz', 'seiz', 'absz', 'tnsz', 'mysz'])
     parser.add_argument('--file_path', type = str)
     parser.add_argument('--seiz_strat', type = eval, default = False)
--- main.py
+++ main.py
@@ -29,6 +29,7 @@ def main(args):
 
     # get datasets and dataloaders
     datagen = config['data_gen']
+    datagen['seed'] = args.seed
     datagen['seiz_classes'] = args.seiz_classes
     datagen['hdf5_path'] = args.file_path
     datagen['window_length'] = args.window_length
@@ -195,6 +196,7 @@ if __name__ == '__main__':
     parser.add_argument('--job_name', type = str, default='nojobname')
     parser.add_argument('--run_folder', type = str, default='notspec')
     # datagen
+    parser.add_argument('--seed', type = int, default = 20)
     parser.add_argument('--seiz_classes', type = eval, default=['fnsz', 'gnsz', 'cpsz', 'spsz', 'tcsz', 'seiz', 'absz', 'tnsz', 'mysz'])
     parser.add_argument('--excl_seiz_classes', type = eval, default = [])
     parser.add_argument('--file_path', type = str)
