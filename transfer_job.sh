9d54f66fa8ad013c0a12a1af0706b0102dd25f19
Thea Brusch <s164238@student.dtu.dk>
2022-02-09 16:17:06 +0100

    local

--- transfer_job.sh
+++ transfer_job.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #BSUB -q gpuv100
 #BSUB -gpu "num=1"
-#BSUB -J transfer_recs_b10_allsubj
+#BSUB -J transfer_recs_bal_allsubj
 ### number of core
 #BSUB -n 1 
 ### specify that all cores should be on the same host
@@ -19,5 +19,5 @@ echo "Runnin script..."
 
 source $HOME/miniconda3/bin/activate
 conda activate semi_active_seiz
-python3 main_transfer.py --run_folder transfer_recs/transfer_val3  --job_name transfer_recs_b10_allsubj --file_path /work3/theb/temple_seiz_full.hdf5 --model_path /zhome/89/a/117273/Desktop/semi_active_seizure/models/checkpoints/excl_val_3/final_model.pt --window_length 2 --bckg_stride 2 --seiz_stride 2 --bckg_rate_train 10 --lr 1e-5 --epochs 100 --weight_decay 1e-2 --glob_avg_pool False --padding False --anno_based_seg True --dropoutprob 0.6 --optimizer RMSprop --use_weighted_loss True --milestones [70,150] --scheduler None --seiz_classes 'fnsz' 'gnsz' 'cpsz' 'spsz' 'tcsz' 'seiz' 'absz' 'tnsz' 'mysz' --batch_size 22 --val_split 3 --split 2 --lambda_cons 0 --use_subjects 'all' --tol 1e-4 --max_recs 30
+python3 main_transfer.py --run_folder transfer_recs/transfer_val3  --job_name transfer_recs_bal_allsubj --file_path /work3/theb/temple_seiz_full.hdf5 --model_path /zhome/89/a/117273/Desktop/semi_active_seizure/models/checkpoints/excl_val_3/final_model.pt --window_length 2 --bckg_stride 2 --seiz_stride 2 --bckg_rate_train 1 --lr 1e-5 --epochs 100 --weight_decay 1e-2 --glob_avg_pool False --padding False --anno_based_seg True --dropoutprob 0.6 --optimizer RMSprop --use_weighted_loss True --milestones [70,150] --scheduler None --seiz_classes 'fnsz' 'gnsz' 'cpsz' 'spsz' 'tcsz' 'seiz' 'absz' 'tnsz' 'mysz' --batch_size 22 --val_split 3 --split 2 --lambda_cons 0 --use_subjects 'all' --tol 1e-4 --max_recs 30
 
