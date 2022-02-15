a2523af25e8b5c049b999d389062ca14cc91dd32
Thea Brusch <s164238@student.dtu.dk>
2022-02-15 09:59:16 +0100

    local changes

--- optuna_job.sh
+++ optuna_job.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #BSUB -q gpuv100
 #BSUB -gpu "num=1"
-#BSUB -J optuna_final_valsplit2_f1_do
+#BSUB -J optuna_final_valsplit0_f1_do
 ### number of core
 #BSUB -n 1 
 ### specify that all cores should be on the same host
@@ -19,4 +19,4 @@ echo "Runnin script..."
 
 source $HOME/miniconda3/bin/activate
 conda activate semi_active_seiz
-python3 cross_val_main.py --run_folder optuna_final_f1val --job_name optuna_final_valsplit_f1val_2_newdo --file_path /work3/theb/temple_seiz_full.hdf5 --n_trials 50 --epochs 100 --time_out 68000 --stride [0.5,1,1.5,2] --bckg_rate [1,2] --load_existing True --seiz_classes 'fnsz' 'gnsz' 'cpsz' 'spsz' 'tcsz' 'seiz' 'absz' 'tnsz' 'mysz' --split 3 --val_split 2 --separate_dropout True
+python3 cross_val_main.py --run_folder optuna_final_f1val --job_name optuna_final_valsplit_f1val_0_newdo --file_path /work3/theb/temple_seiz_full.hdf5 --n_trials 50 --epochs 100 --time_out 58000 --stride [0.5,1,1.5,2] --bckg_rate [1,2] --load_existing True --seiz_classes 'fnsz' 'gnsz' 'cpsz' 'spsz' 'tcsz' 'seiz' 'absz' 'tnsz' 'mysz' --split 3 --val_split 0 --separate_dropout True
