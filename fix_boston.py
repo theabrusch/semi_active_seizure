7a8329740fa413da6568b321709f1d073087082f
theb <s164238@student.dtu.dk>
2022-02-19 00:33:36 +0100

    add print

--- fix_boston.py
+++ fix_boston.py
@@ -2,7 +2,10 @@ from dataapi import data_collection as dc
 
 F = dc.File('/work3/theb/boston_scalp_newnew.hdf5', 'r+')
 records = F.get_children(dc.Record, get_obj=False)
+i=1
 for rec in records:
+    print(i, 'of', len(records))
+    i+=1
     record = F[rec]
     annotations = record['Annotations']
     if len(record['Annotations']) > 0:
