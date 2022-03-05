c7475fba05088bbb48af7ba9e08585ea3c426e45
Thea Brusch <s164238@student.dtu.dk>
2022-03-05 12:44:48 +0100

    local changes

--- analysis_job.sh
+++ analysis_job.sh
@@ -19,4 +19,4 @@ echo "Runnin script..."
 
 source $HOME/miniconda3/bin/activate
 conda activate semi_active_seiz
-python3 main_analysis.py --run_folder kfold_temple_tcsz --job_name fnsz_eval --file_path /work3/theb/temple_seiz_full.hdf5 --window_length 2 --glob_avg_pool False --padding True --dropoutprob 0.4709 --cnn_dropoutprob 0.3246 --seiz_classes 'fnsz' --split 3 --n_splits 5 --onlytrainseiz None --orig_split True --model_path '/zhome/89/a/117273/Desktop/semi_active_seizure/models/final_models/2022-03-01 13:59:16.444104finalsplit_test_fnsz_valsplit_both/final_model_105_sensspec.pt'
+python3 main_analysis.py --run_folder kfold_temple_tcsz --job_name fnsz_eval_f1 --file_path /work3/theb/temple_seiz_full.hdf5 --window_length 2 --glob_avg_pool False --padding True --dropoutprob 0.4709 --cnn_dropoutprob 0.3246 --seiz_classes 'fnsz' --split 3 --n_splits 5 --onlytrainseiz None --orig_split True --model_path '/zhome/89/a/117273/Desktop/semi_active_seizure/models/final_models/2022-03-01 13:59:16.444104finalsplit_test_fnsz_valsplit_both/final_model_105_f1.pt'
